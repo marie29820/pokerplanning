7540c605a4f517542ca3bbc138613d515b32d9ab
cs525 <cedric.guillot@partnre.Com>
2023-01-24 16:51:35 +0100

    add https backend

--- src/service/api.js
+++ src/service/api.js
@@ -2,7 +2,7 @@ import SockJS from "sockjs-client";
 import {Stomp} from "@stomp/stompjs";
 import {messageStore} from "@/store";
 
-const stomp = "http://44.207.3.141:8080/pokerplanning/stomp"
+const stomp = "https://44.207.3.141/pokerplanning/stomp"
 const roomTopic = "/topic/room/"
 
 let stompClient = null;
