c55b04efd1249399671e459132e12b4b81901d89
marie29820 <94717097+marie29820@users.noreply.github.com>
2023-01-24 14:52:46 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   chainWebpack: (config) => {
+    publicPath: process.env.NODE_ENV === 'production' ? '/pokerplanning/' : '/'
     config.resolve.alias.set('vue', '@vue/compat')
     config.module
       .rule('vue')
